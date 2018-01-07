55db220cffeac6c62ddec07573fb036a418a0dca
EndangeredFish <zwy346545141@gmail.com>
2018-01-07 18:10:41 +0800

    args in file

--- NetworkAuth.py
+++ NetworkAuth.py
@@ -3,30 +3,33 @@ import requests
 import urllib
 import getpass
 from urllib.parse import urlparse
+from config import username
+from config import password
+from config import serv
 
 
 class NotRouterError(ValueError):
     pass
 
 
-def servicechoose(serv):
-    try:
-        if serv == '1':
-            return "default"
-        elif serv == '2':
-            return "unicom"
-        elif serv == '3':
-            return "cmcc"
-        elif serv == '4':
-            return "ctcc"
-        elif serv == '5':
-            return "local"
-        else:
-            print("Input failed!")
-    except Exception:
-        print("输入类型有误！请重新运行程序")
-        exit(1)
-
+# def servicechoose(serv):
+#     try:
+#         if serv == '1':
+#             return "default"
+#         elif serv == '2':
+#             return "unicom"
+#         elif serv == '3':
+#             return "cmcc"
+#         elif serv == '4':
+#             return "ctcc"
+#         elif serv == '5':
+#             return "local"
+#         else:
+#             print("Input failed!")
+#     except Exception:
+#         print("输入类型有误！请重新运行程序")
+#         exit(1)
+#
 
 argParsed = ""
 address = "http://121.251.251.207"
@@ -51,21 +54,21 @@ else:
 
 if argParsed.find('wlanuserip') == -1:
     print("你已经登录！")
-    userIndex = urlparse(trueUrl).query[10:]
-    if input("输入0退出登录，其他输入则忽略: ") == '0':
-        requests.post("http://121.251.251.217/eportal/InterFace.do?method=logout", data={'userIndex': userIndex})
-        requests.post("http://121.251.251.207/eportal/InterFace.do?method=logout", data={'userIndex': userIndex})
-        print("退出成功！")
-        exit(0)
-    else:
-        print("保持登录！")
-        exit(0)
+    # userIndex = urlparse(trueUrl).query[10:]
+    # if input("输入0退出登录，其他输入则忽略: ") == '0':
+    #     requests.post("http://121.251.251.217/eportal/InterFace.do?method=logout", data={'userIndex': userIndex})
+    #     requests.post("http://121.251.251.207/eportal/InterFace.do?method=logout", data={'userIndex': userIndex})
+    #     print("退出成功！")
+    #     exit(0)
+    # else:
+    #     print("保持登录！")
+    #     exit(0)
 url = address+"/eportal/InterFace.do?method=login"
-userName = input("输入你的数字石大账户：")
-passWord = getpass.getpass("输入你的数字石大密码：")
-service = servicechoose(input("输入运营商(1为校园网，2为联通，3为移动，4为电信,5为校园内网):"))
-payload = {'userId': userName, 'password':passWord, 'service': service, 'queryString': argParsed,'operatorPwd': '', 'operatorUserId': '', 'vaildcode': ''}
-postMessage = requests.post(url,data=payload)
+userName = username
+passWord = password
+service = serv
+payload = {'userId': userName, 'password': passWord, 'service': service, 'queryString': argParsed, 'operatorPwd': '', 'operatorUserId': '', 'vaildcode': ''}
+postMessage = requests.post(url, data=payload)
 # print(postMessage.text)
 if postMessage.text.find("success") >= 0:
     print("登陆成功")
