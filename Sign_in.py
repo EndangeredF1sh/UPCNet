c4f6bbeb1f6bbe5f4c40e487b124651c0d21850b
LucienShui <18952904@qq.com>
2018-03-19 13:40:39 +0800

    0.0.3

--- Sign_in.py
+++ Sign_in.py
@@ -19,19 +19,19 @@ try:
     if trueText.find("http://121.251.251.217") > 0 and trueUrl.find("http://www.upc.edu.cn") == 0:
         raise NotRouterError()
 except requests.exceptions.ConnectionError:
-    print("网络连接故障,请检查你的网络连接或关闭系统弹出认证窗口！")
+    print("网络连接故障,请检查网络连接或关闭系统弹出的认证窗口!")
     exit(-1)
 except NotRouterError:
     address = "http://121.251.251.217/"
     pIndex = requests.get(address, allow_redirects=True).text.find('wlanuserip')
     argParsed = urllib.parse.quote(trueText[pIndex:])
-    print("你连接的是UPC热点!")
+    print("当前连接的是UPC热点")
 
 else:
     argParsed = urllib.parse.quote(urlparse(trueUrl).query)
 
 if argParsed.find('wlanuserip') == -1:
-    print("你已经登录！")
+    print("当前状态为已登录")
     exit(0)
 
 url = address+"/eportal/InterFace.do?method=login"
