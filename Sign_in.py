caaa018b4bdc1bb6d672d3149e12062ecd587632
LucienShui <18952904@qq.com>
2018-03-19 13:34:19 +0800

    1.1

--- Sign_in.py
+++ Sign_in.py
@@ -1,26 +1,47 @@
 import requests
 import urllib
-import time
+import getpass
 from urllib.parse import urlparse
-def servicechoose(serv):
-    if serv == '1': return "default"
-    elif serv == '2': return "unicom"
-    elif serv == '3': return "cmcc"
-    elif serv == '4': return "ctcc"
-    return "local"
-print("By LucienShui & EndangeredF1sh")
-print("------------------------------")
-trueUrl = requests.post("http://121.251.251.207", allow_redirects=True).url
-argParsed = urllib.parse.quote(urlparse(trueUrl).query)
-if argParsed.find('wlanuserip') == -1: 
-	print("您已登录")
-	time.sleep(1)
-	exit(0)
-str = open("./in.txt").readline()
-url = "http://121.251.251.207/eportal/InterFace.do?method=login"
-payload = {'userId': str.split(' ')[0], 'password': str.split(' ')[1], 'service': servicechoose(str.split(' ')[2]), 'queryString': argParsed,'operatorPwd': '', 'operatorUserId': '', 'vaildcode': ''}
-postMessage = requests.post(url,data = payload)
-if postMessage.text.find("success") >= 0: print("登陆成功")
-else: print("登陆失败")
-time.sleep(1)
-exit(0)
+from config import username
+from config import password
+from config import serv
+
+
+class NotRouterError(ValueError):
+    pass
+
+argParsed = ""
+address = "http://121.251.251.207"
+try:
+    trueUrl = requests.post("http://121.251.251.217", allow_redirects=True).url
+    trueText = requests.get("http://121.251.251.217", allow_redirects=True).text
+
+    if trueText.find("http://121.251.251.217") > 0 and trueUrl.find("http://www.upc.edu.cn") == 0:
+        raise NotRouterError()
+except requests.exceptions.ConnectionError:
+    print("网络连接故障,请检查你的网络连接或关闭系统弹出认证窗口！")
+    exit(-1)
+except NotRouterError:
+    address = "http://121.251.251.217/"
+    pIndex = requests.get(address, allow_redirects=True).text.find('wlanuserip')
+    argParsed = urllib.parse.quote(trueText[pIndex:])
+    print("你连接的是UPC热点!")
+
+else:
+    argParsed = urllib.parse.quote(urlparse(trueUrl).query)
+
+if argParsed.find('wlanuserip') == -1:
+    print("你已经登录！")
+    exit(0)
+
+url = address+"/eportal/InterFace.do?method=login"
+userName = username
+passWord = password
+service = serv
+payload = {'userId': userName, 'password': passWord, 'service': service, 'queryString': argParsed, 'operatorPwd': '', 'operatorUserId': '', 'vaildcode': ''}
+postMessage = requests.post(url, data=payload)
+if postMessage.text.find("success") >= 0:
+    print("登陆成功")
+else:
+    print("登陆失败")
+    exit(0)
---
+++ config.py
@@ -0,0 +1,4 @@
+username = "" # 用户名
+password = "" # 密码
+serv = "" # 运营商
+# default、unicom、cmcc、ctcc、local，分别对应：校园网、联通、移动、电信、校园内网
