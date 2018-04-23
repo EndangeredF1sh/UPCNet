12ef308eea6238e63428b2d09f409d2e49c68e74
Lucien <18952904@qq.com>
2018-04-23 13:36:26 +0800

    1.0.3
    优化了文件写入步骤，增强了添加账号时的易读性

--- UPCNet.py
+++ UPCNet.py
@@ -4,17 +4,17 @@ import os
 import base64
 import time
 
-def servicechoose(serv): # 运营商选择
-    if serv == '1': return "default" # 校园网
-    elif serv == '2': return "unicom" # 联通
-    elif serv == '3': return "cmcc" # 移动
-    elif serv == '4': return "ctcc" # 电信
-    return "local" # 校园内网
+def servicechoose(serv):  # 运营商选择
+    if serv == '1': return "default"  # 校园网
+    elif serv == '2': return "unicom"  # 联通
+    elif serv == '3': return "cmcc"  # 移动
+    elif serv == '4': return "ctcc"  # 电信
+    return "local"  # 校园内网
 
-def encode(string): # 加密
+def encode(string):  # 加密
     return base64.encodebytes(str.encode(string, 'utf-8'))
 
-def decode(code): # 解密
+def decode(code):  # 解密
     return bytes.decode(base64.decodebytes(code), 'utf-8')
 
 def autoexit():
@@ -25,11 +25,11 @@ def autoexit():
 class NotRouterError(ValueError):
     pass
 
-filename = './config.ini' # 加密后的账号密码储存在根目录下
+filename = './config.ini'  # 加密后的账号密码储存在根目录下
 
-def login(): # 登录模块
+def login():  # 登录模块
     argParsed = ""
-    address = "http://121.251.251.207" # 默认尝试进行有线登录
+    address = "http://121.251.251.207"  # 默认尝试进行有线登录
     try:
         trueUrl = requests.post("http://121.251.251.217", allow_redirects=True).url
         trueText = requests.get("http://121.251.251.217", allow_redirects=True).text
@@ -38,7 +38,8 @@ def login(): # 登录模块
             raise NotRouterError()
 
     except requests.exceptions.ConnectionError:
-        print("Please check the network connection or close the login windows")  # macOS的登录界面会阻断网络连接
+        # macOS的登录界面会阻断网络连接
+        print("Please check the network connection or close the login windows")
         autoexit()
 
     except NotRouterError:
@@ -55,7 +56,7 @@ def login(): # 登录模块
 
     url = address + "/eportal/InterFace.do?method=login"
 
-    str = decode(open(filename, "rb").readline()) # 读取二进制文件并解密
+    str = decode(open(filename, "rb").readline())  # 读取二进制文件并解密
     userName = str.split(' ')[0]
     passWord = str.split(' ')[1]
     service = servicechoose(str.split(' ')[2])
@@ -63,22 +64,17 @@ def login(): # 登录模块
     payload = {'userId': userName, 'password': passWord, 'service': service, 'queryString': argParsed,
                'operatorPwd': '', 'operatorUserId': '', 'vaildcode': ''}
     postMessage = requests.post(url, data=payload)
-    
-    if postMessage.text.find("success") >= 0:
-        print("Login success")  # 登录成功
-    else:
-        print("Something wrong")  # 登录失败
+
+    if postMessage.text.find("success") >= 0: print("Login success")  # 登录成功
+    else: print("Something wrong")  # 登录失败
     autoexit()
 
-if os.path.exists(filename):
-    login()
+if os.path.exists(filename): login()
 else:
     print('No user data, please input your account imformation.')
-    with open(filename, 'wb') as file: # 二进制文件写入
-        # 把账号信息压进一个字符串后进行加密
-        str_tmp = input('School number:\n')
-        str_tmp = str_tmp + ' ' + input('Password:\n')
-        str_tmp = str_tmp + ' ' + input('Communications:\n1.default 2.unicom 3.cmcc 4.ctcc 5.local\n')
-        code = encode(str_tmp) # 加密
-        file.write(code) # 写文件
+    str_tmp = input('School number: ') # 把账号信息压进一个字符串后进行加密
+    str_tmp = str_tmp + ' ' + input('Password: ')
+    str_tmp = str_tmp + ' ' + input('=========\n1.default\n2.unicom\n3.cmcc\n4.ctcc\n5.local\n'
+                                    '=========\nCommunications number: ')
+    open(filename, 'wb').write(encode(str_tmp)) # 加密后的字符串写入二进制文件
     login()
