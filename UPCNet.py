4d489644b87be45930ecd90fdf9dfcb9adc518d5
LucienShui <18952904@qq.com>
2018-07-02 03:05:08 +0800

    增加了联网状态判断，如果是假联网状态，那么就会先从学校断开连接，然后再重新连接，优化了逻辑结构，增加了密码保护。

--- UPCNet.py
+++ UPCNet.py
@@ -1,8 +1,13 @@
-import requests
-import urllib
-import os
-import base64
-import time
+import requests, urllib, os, base64, time, sys
+from getpass import getpass
+
+# 全局变量
+
+filePath = sys.path[0] + '/config.ini'  # 加密后的账号密码储存在根目录下
+argParsed = ""
+trueText = ""
+trueUrl = ""
+
 
 def servicechoose(serv):  # 运营商选择
     if serv == '1': return "default"  # 校园网
@@ -11,17 +16,29 @@ def servicechoose(serv):  # 运营商选择
     elif serv == '4': return "ctcc"  # 电信
     return "local"  # 校园内网
 
+
 def encode(string):  # 加密
     return base64.encodebytes(str.encode(string, 'utf-8'))
+
 
 def decode(code):  # 解密
     return bytes.decode(base64.decodebytes(code), 'utf-8')
 
-def autoexit():
-    time.sleep(1)
-    exit(0)
 
-class fontType: # 控制台字体输出样式
+def autoexit(code):
+    time.sleep(1)
+    exit(code)
+
+
+def online():
+    try:
+        query = requests.get("http://www.lucien.ink/test", timeout=5)
+        return query.text == 'Hello World!'
+    except:
+        return False
+
+
+class fontType:  # 控制台字体输出样式
     underLine = '\033[4m'
     red = '\033[31m'
     green = '\033[32m'
@@ -29,25 +46,26 @@ class fontType: # 控制台字体输出样式
     tail = '\033[0m'
     clear = '\033[1A\033[K'
 
+
 class NotRouterError(ValueError):
     pass
 
-filename = './config.ini'  # 加密后的账号密码储存在根目录下
 
 def login():  # 登录模块
-    argParsed = ""
+    global argParsed, trueText, trueUrl
     address = "http://121.251.251.207"  # 默认尝试进行有线登录
     try:
         trueUrl = requests.post("http://121.251.251.217", allow_redirects=True).url
         trueText = requests.get("http://121.251.251.217", allow_redirects=True).text
 
-        if trueText.find("http://121.251.251.217") > 0 and trueUrl.find("http://www.upc.edu.cn") == 0:
+        # if trueText.find("http://121.251.251.217") > 0 and trueUrl.find("http://www.upc.edu.cn") == 0:
+        if trueText.find("http://121.251.251.217") > 0:
             raise NotRouterError()
 
     except requests.exceptions.ConnectionError:
         # macOS的登录界面会阻断网络连接
         print("Please check the network connection or close the login windows")
-        autoexit()
+        autoexit(1)
 
     except NotRouterError:
         address = "http://121.251.251.217/"
@@ -58,12 +76,22 @@ def login():  # 登录模块
         argParsed = urllib.parse.quote(urllib.parse.urlparse(trueUrl).query)
 
     if argParsed.find('wlanuserip') == -1:
-        print("Already online")  # 已经登录
-        autoexit()
+        if online():
+            print("Already online")  # 已经登录
+            autoexit(0)
+        else:
+            userIndex = urllib.parse.urlparse(trueUrl).query[10:]
+            if address.find('217'):
+                requests.post("http://121.251.251.217/eportal/InterFace.do?method=logout",
+                              data={'userIndex': userIndex})
+            else:
+                requests.post("http://121.251.251.207/eportal/InterFace.do?method=logout",
+                              data={'userIndex': userIndex})
+            login()
 
     url = address + "/eportal/InterFace.do?method=login"
 
-    str = decode(open(filename, "rb").readline())  # 读取二进制文件并解密
+    str = decode(open(filePath, "rb").readline())  # 读取二进制文件并解密
     userName = str.split(' ')[0]
     passWord = str.split(' ')[1]
     service = servicechoose(str.split(' ')[2])
@@ -72,17 +100,48 @@ def login():  # 登录模块
                'operatorPwd': '', 'operatorUserId': '', 'vaildcode': ''}
     postMessage = requests.post(url, data=payload)
 
-    if postMessage.text.find("success") >= 0: print("Login success")  # 登录成功
-    else: print("Something wrong")  # 登录失败
-    autoexit()
+    if postMessage.text.find("success") >= 0:
+        print("Login success")  # 登录成功
+    else:
+        print("Something wrong")  # 登录失败
+    autoexit(0)
 
-if os.path.exists(filename): login()
-else:
-    print(fontType.red + 'When using for the first time, please complete the imformation first.' + fontType.red)
-    # 把账号信息压进一个字符串后进行加密
-    str_tmp = input(fontType.green + 'School number' + fontType.tail + ': ')
-    str_tmp = str_tmp + ' ' + input(fontType.green + 'Password' + fontType.tail + ': ')
-    str_tmp = str_tmp + ' ' + input('1.default\n2.unicom\n3.cmcc\n4.ctcc\n5.local\n' 
-            + fontType.green + 'Communications number' + fontType.tail + ': ')
-    open(filename, 'wb').write(encode(str_tmp)) # 加密后的字符串写入二进制文件
+
+def upcnet():
+    global argParsed, trueText, trueUrl
+    argParsed = ""
+    trueText = ""
+    trueUrl = ""
+    if not os.path.exists(filePath):
+        # print(fontType.red + 'When using for the first time, please complete the imformation first' + fontType.red)
+        # 把账号信息压进一个字符串后进行加密
+        str_tmp = input(fontType.red + 'School number' + fontType.tail + ': ')
+        # str_tmp = str_tmp + ' ' + input(fontType.green + 'Password(Invisible)' + fontType.tail + ': ')
+        str_tmp = str_tmp + ' ' + getpass(fontType.red + 'Password' + fontType.tail + ':(Invisible) ')
+        str_tmp = str_tmp + ' ' + input('1.default\n2.unicom\n3.cmcc\n4.ctcc\n5.local\n'
+                                        + fontType.red + 'Communications number' + fontType.tail + ': ')
+        open(filePath, 'wb').write(encode(str_tmp))  # 加密后的字符串写入二进制文件
+
     login()
+
+
+def main():
+    if len(sys.argv) > 1:
+        if (len(sys.argv) > 2):
+            print('Too many args')
+            autoexit(1)
+
+        if sys.argv[1] == 'reset':
+            if os.path.exists(filePath):
+                os.remove(filePath)
+            print('Reset successful')
+            autoexit(0)
+
+        print('Wrong args')
+        autoexit(1)
+
+    upcnet()
+
+
+if __name__ == '__main__':
+    main()
