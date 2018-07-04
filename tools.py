cb7ceb870a0456b790b93819621f71e82e2f6be3
Lucien <lucien@lucien.ink>
2018-07-05 01:34:39 +0800

    各种辅助程序

--- tools.py
+++ tools.py
@@ -25,7 +25,7 @@ def decode(code):  # 解密
     return bytes.decode(base64.decodebytes(code), 'utf-8')
 
 
-def autoexit():
+def autoexit(): # 延时一秒后结束程序
     time.sleep(1)
     sys.exit()
 
@@ -34,6 +34,6 @@ def getpath():
     return os.path.split(os.path.realpath(__file__))[0] + '\config.ini'  # 加密后的账号密码储存在根目录下
 
 
-def online():
-    text = requests.get("https://api.zhihu.com/test", allow_redirects=True).text
+def online(): # （已弃用）测试当前是否有外网
+    text = requests.get("https://api.zhihu.com/test", allow_redirects=True).text # 对知乎发请求，存储返回值
     return text.find('400') != -1
