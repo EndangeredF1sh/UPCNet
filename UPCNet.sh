968158357e91a58f13ba4840f6c48ea1f16b47bf
EndangeredFish <im.EndangeredFish@gmail.com>
2020-11-13 14:55:46 +0800

    fix 'curl: Couldn't resolve host xxx.xxx'
    DNS record cannot be queried without authorization. (Due to new policy in UPC)
    Hardcode the authorization url. (http://121.251.251.207/eportal/InterFace.do?method=login) It may not work in wlan environment, checking required.
    Improved url cut algorithm, now it can cut both wlan.upc.edu.cn and lan.upc.edu.cn correctly.

--- UPCNet.sh
+++ UPCNet.sh
@@ -19,9 +19,9 @@ echo $DATE network restart >>my_watchdog.log
 /etc/init.d/network restart
 sleep 10
 parameter='userId='${username}'&password='${password}'&service='${service}'&queryString='
-location='http://lan.upc.edu.cn/eportal/InterFace.do?method=login'
+location='http://121.251.251.207/eportal/InterFace.do?method=login'
 url=`curl -Ls -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36" -o /dev/null -w %{url_effective} http://121.251.251.217`
-url=${url#http://lan.upc.edu.cn/eportal/index.jsp?}
+url=${url#*/eportal/index.jsp?}
 url=${url//=/%253D}
 url=${url//&/%2526}
 url=${url//:/%253A}
