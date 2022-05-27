ac7f9cf48d8818bbb69af20068bbd8ba3b7ec4cc
HowieWu <98788152+howie-wu@users.noreply.github.com>
2022-05-27 18:19:17 +0800

    Update autoinstall.sh
    updata Backup

--- autoinstall.sh
+++ autoinstall.sh
@@ -20,6 +20,8 @@ echo "Auto install start ..."
 opkg update
 opkg install curl
 wget --no-check-certificate https://raw.githubusercontent.com/howie-wu/UPCNet/bash/UPCNet.sh && chmod +x UPCNet.sh
+echo -n "If Backup (1 for no,2 for yes) -> "
+read bk
 echo -n "input stuID -> "
 read stuID
 echo -n "input password -> "
@@ -56,14 +58,56 @@ read -p "Enter selection [1-5] -> " num
 	if [[ $num == 5 ]]; then
 		service="local"
 	fi
+if [ $bk == 2 ]
+then
+echo -n "input stuID -> "
+read stuIDB
+echo -n "input password -> "
+read passwdB
+echo "Select your back service:
+1. Campus Network Service
+2. China Unicom Network Service
+3. China Mobile Network Service
+4. China Telecom Network Service
+5. Campus Intranet
+"
+read -p "Enter selection [1-5] -> " num
+	if [ $num -lt 1 ] || [ $num -gt 5 ];then
+	echo "invalid selection, exit."; 
+	exit;
+	fi
+ 
+	if [[ $num == 1 ]]; then
+		serviceB="default"
+	fi
+	
+	if [[ $num == 2 ]]; then
+		serviceB="unicom"
+	fi
+	
+	if [[ $num == 3 ]]; then
+		serviceB="cmcc"
+	fi
+	
+	if [[ $num == 4 ]]; then
+		serviceB="ctcc"
+	fi
 
-
+	if [[ $num == 5 ]]; then
+		serviceB="local"
+	fi
+fi
 sed -i '1,3d' UPCNet.sh
+sed -i "1i back=\"$bk\"" UPCNet.sh
 sed -i "1i service=\"$service\"" UPCNet.sh
 sed -i "1i password=\"$passwd\"" UPCNet.sh
 sed -i "1i username=\"$stuID\"" UPCNet.sh
-
-
+if [ $bk == 2 ]
+then
+sed -i "1i serviceB=\"$service\"" UPCNet.sh
+sed -i "1i passwordB=\"$passwdB\"" UPCNet.sh
+sed -i "1i usernameB=\"$stuIDB\"" UPCNet.sh
+fi
 croncmd="sh ${SHELL_FOLDER}"/UPCNet.sh""
 cronjob="*/1 * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
