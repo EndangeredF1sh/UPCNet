d69beff51eb9b5981f8458cee331bcd9a133232c
HowieWu <98788152+howie-wu@users.noreply.github.com>
2022-05-27 18:13:11 +0800

    Update UPCNet.sh
    Add Back

--- UPCNet.sh
+++ UPCNet.sh
@@ -1,6 +1,12 @@
+back=""
+flag="2"
 username=""
 password=""
 service=""
+
+usernameB=""
+passwordB=""
+serviceB=""
 DATE=`date +%Y-%m-%d-%H:%M:%S`
 tries=0
 echo --- my_watchdog start ---
@@ -17,8 +23,12 @@ done
 
 echo $DATE network restart >>my_watchdog.log
 /etc/init.d/network restart
-sleep 10
+sleep 5
 parameter='userId='${username}'&password='${password}'&service='${service}'&queryString='
+if [ $back == $flag ]
+then
+parameterB='userId='${username}'&password='${password}'&service='${service}'&queryString='
+fi
 location='http://121.251.251.207/eportal/InterFace.do?method=login'
 url=`curl -Ls -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36" -o /dev/null -w %{url_effective} http://121.251.251.217`
 url=${url#*/eportal/index.jsp?}
@@ -27,5 +37,15 @@ url=${url//&/%2526}
 url=${url//:/%253A}
 url=${url//\//%252F}
 parameter=${parameter}${url}'&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false'
+if [ $back == $flag ]
+then
+parameterB=${parameterB}${url}'&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false'
+echo $parameterB
+fi
+
 echo $parameter
 curl -X POST -d $parameter $location
+if [ $back == $flag ]
+then
+curl -X POST -d $parameterB $location
+fi
