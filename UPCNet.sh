38353424413c528e9a2d5baab004b641e76c2399
HowieWu <98788152+how1ewu@users.noreply.github.com>
2023-12-12 21:12:23 +0800

    Update UPCNet.sh

--- UPCNet.sh
+++ UPCNet.sh
@@ -1,28 +1,4 @@
 cat=""
-back=""
-username=""
-password=""
-service=""
-usernameB=""
-passwordB=""
-serviceB=""
-
-DATE=`date +%Y-%m-%d-%H:%M:%S`
-echo "Start detecting network connection status......"
-ip_list="119.29.29.29,223.5.5.5"
-ips=$(echo "$ip_list" | tr ',' ' ')
-for ip in $ips
-do
-        if /bin/ping -c 1 "$ip" >/dev/null
-        then
-                echo "The network connection is normal, enjoy~"
-                echo "---close script---"
-                exit 0
-        fi
-done
-sleep 1
-echo "!!!ERROR: Network connection exception!!!"
-echo "Start writing to the log and execute the login script..."
 echo $DATE network offline and try to restart... ... >>my_watchdog.log
 # /etc/init.d/network restart
 # 重启网卡需要吗？
@@ -33,7 +9,7 @@ then
         uci commit openclash
         /etc/init.d/openclash stop
         sleep 10
-done
+fi
 parameter='userId='${username}'&password='${password}'&service='${service}'&queryString='
 location='http://121.251.251.207/eportal/InterFace.do?method=login'
 url=`curl -Ls -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36" -o /dev/null -w %{url_effective} http://121.251.251.217`
@@ -63,9 +39,9 @@ do
                         uci set openclash.config.enable='1'
                         uci commit openclash
                         /etc/init.d/openclash start
-                        sleep 30
-                done
-                exit 0
+                        sleep 40
+                        reboot
+                fi
         fi
 done
 echo -e "FAILED, Start trying to log in with an alternate account\n"
@@ -87,9 +63,9 @@ do
                         uci set openclash.config.enable='1'
                         uci commit openclash
                         /etc/init.d/openclash start
-                        sleep 30
-                done
-                exit 0
+                        sleep 40
+                        reboot
+                fi
         fi
 done
 echo "You can't sign in with your existing account. Please check your configuration and other network settings and close script"
