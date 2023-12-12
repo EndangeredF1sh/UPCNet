78927538eae09858fbc362af9f96bae85f0c2ebf
HowieWu <98788152+how1ewu@users.noreply.github.com>
2023-12-12 20:20:59 +0800

    Update UPCNet.sh

--- UPCNet.sh
+++ UPCNet.sh
@@ -7,24 +7,19 @@ passwordB=""
 serviceB=""
 
 DATE=`date +%Y-%m-%d-%H:%M:%S`
-tries=0
 echo "Start detecting network connection status......"
-while [[ $tries -lt 2 ]]
+ip_list="119.29.29.29,223.5.5.5"
+ips=$(echo "$ip_list" | tr ',' ' ')
+for ip in $ips
 do
-        ip_list="119.29.29.29,223.5.5.5"
-        ips=$(echo "$ip_list" | tr ',' ' ')
-        for ip in $ips
-        do
-                if /bin/ping -c 1 "$ip" >/dev/null
-                then
-                        echo "The network connection is normal, enjoy~"
-                        echo "---close script---"
-                        exit 0
-                fi
-        done
-        tries=$((tries+1))
-        sleep 1
+        if /bin/ping -c 1 "$ip" >/dev/null
+        then
+                echo "The network connection is normal, enjoy~"
+                echo "---close script---"
+                exit 0
+        fi
 done
+sleep 1
 echo "!!!ERROR: Network connection exception!!!"
 echo "Start writing to the log and execute the login script..."
 echo $DATE network offline and try to restart... ... >>my_watchdog.log
@@ -46,47 +41,40 @@ curl -X POST -d $parameter $location
 sleep 8
 if [ $back == 2 ]
 then
-triesagain=0
-
-while [[ $triesagain -lt 2 ]]
+ip_list="119.29.29.29,223.5.5.5"
+ips=$(echo "$ip_list" | tr ',' ' ')
+for ip in $ips
 do
-        ip_list="119.29.29.29,223.5.5.5"
-        ips=$(echo "$ip_list" | tr ',' ' ')
-        for ip in $ips
-        do
-                if /bin/ping -c 1 "$ip" >/dev/null
-                then
-                        echo -e "\n"
-                        echo "Log in with your primary account, the network is back to normal, enjoy~"
-                        echo "---close script---"
-                        exit 0
-                fi
-        done
-        tries=$((triesagain+1))
-        sleep 1
+        if /bin/ping -c 1 "$ip" >/dev/null
+        then
+                echo -e "\n"
+                echo "Log in with your primary account, the network is back to normal, enjoy~"
+                echo "---close script---"
+                reboot
+                exit 0
+        fi
+done
+sleep 1
 done
 echo -e "FAILED, Start trying to log in with an alternate account\n"
 parameterB='userId='${usernameB}'&password='${passwordB}'&service='${serviceB}'&queryString='
 parameterB=${parameterB}${url}'&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false'
 # echo $parameterB
 curl -X POST -d $parameterB $location
-triesagain2=0
-while [[ $triesagain2 -lt 2 ]]
+ip_list="119.29.29.29,223.5.5.5"
+ips=$(echo "$ip_list" | tr ',' ' ')
+for ip in $ips
 do
-        ip_list="119.29.29.29,223.5.5.5"
-        ips=$(echo "$ip_list" | tr ',' ' ')
-        for ip in $ips
-        do
-                if /bin/ping -c 1 "$ip" >/dev/null
-                then
-                        echo -e "\n"
-                        echo "Log in with your alternate account, the network is back to normal, enjoy~"
-                        echo "---close script---"
-                        exit 0
-                fi
+        if /bin/ping -c 1 "$ip" >/dev/null
+        then
+                echo -e "\n"
+                echo "Log in with your alternate account, the network is back to normal, enjoy~"
+                echo "---close script---"
+                reboot
+                exit 0
+        fi
         done
-        tries=$((triesagain2+1))
-        sleep 1
+sleep 1
 done
 echo "You can't sign in with your existing account. Please check your configuration and other network settings and close script"
 fi
